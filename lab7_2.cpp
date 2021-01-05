b849906e4d8d035130dd4c0a6c460cb82de2b89b
peela123 <peelaxv@gmail.com>
2021-01-05 17:56:14 +0700

    งานlab7

---
+++ lab7_2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+char findGrade(double grade)
+{
+	if (grade > 90)
+		return 'A';
+	else if (grade > 75)
+		return 'B';
+	else if (grade > 60)
+		return 'C';
+	else if (grade > 45)
+		return 'D';
+	else
+		return 'F';
+}
+
+int main()
+{
+	int N, i = 0;
+	cout << "Enter the number of students: ";
+	cin >> N;
+	string name[N];
+	float score[N];
+
+	while (i < N)
+	{
+		cout << "Name of student " << i + 1 << ": ";
+		cin.ignore();
+		getline(cin, name[i]);
+
+		cout << "Score of student " << i + 1 << ": ";
+		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
+		i++;
+	}
+
+	//Print names scores and grades
+	i = 0;
+	cout << "---------------------------------------------\n";
+	cout << setw(25) << "Name" << setw(8) << "Score" << setw(8) << "Grade"
+		 << "\n";
+	cout << "---------------------------------------------\n";
+	while (i < N)
+	{
+		cout << setw(25) << name[i] << setw(8) << score[i] << setw(8) << findGrade(score[i]) << "\n";
+		i++;
+	}
+	cout << "---------------------------------------------\n";
+
+	return 0;
+}
---
+++ lab7_3.cpp
@@ -0,0 +1,66 @@
+/*
+"Enter your age: "
+"Enter your height: "
+"Enter your property: "
+"Your status = "
+"UNFRIEND"
+"FRIEND"
+"BEST FRIEND"
+"ONE-NIGHT-STAND"
+"MARRIED"
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age, property, height;
+    string status;
+    cout << "Enter your age: ";
+    cin >> age;
+    if (age <= 20)
+    {
+        cout << "Enter your height: ";
+        cin >> height;
+        if (height < 160)
+        {
+            status = "UNFRIEND";
+        }
+        else if (height < 175)
+        {
+            status = "FRIEND";
+        }
+        else
+        {
+            cout << "Enter your property: ";
+            cin >> property;
+            if (property > 69000000)
+            {
+                status = "MARRIED";
+            }
+            else
+            {
+                status = "ONE-NIGHT-STAND";
+            }
+        }
+    }
+    else if (age < 30)
+    {
+        cout << "Enter your property: ";
+        cin >> property;
+        if (property > 10000000)
+        {
+            status = "BEST FRIEND";
+        }
+        else
+        {
+            status = "UNFRIEND";
+        }
+    }
+    else
+    {
+        status = "UNFRIEND";
+    }
+    cout << "Your status = " << status << endl;
+    return 0;
+}
